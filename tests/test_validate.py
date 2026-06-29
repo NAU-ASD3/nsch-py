87a8e02aecb532ccc145c24626619789126b0c4a
Sakina <slord.bhs@gmail.com>
2026-06-29 10:40:20 -0700

    check_na_rates: Add schema to DataFrames for clearer types in testing

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -19,7 +19,13 @@ def test_computes_correct_NA_rates_per_year():
             "year": [2016, 2016, 2017, 2017],
             "na_rate": [0.5, 1.0, 0.0, 0.0],
             "n_total": [2, 2, 2, 2],
-        }
+        },
+        schema={
+            "variable": pl.Utf8,
+            "year": pl.Int64,
+            "na_rate": pl.Float64,
+            "n_total": pl.Int64,
+        },
     )
     # Ensure frames are comparing the correct rows to each other
     assert_frame_equal(result.sort(by=["variable", "year"]), expected.sort(by=["variable", "year"]))
@@ -27,6 +33,7 @@ def test_computes_correct_NA_rates_per_year():
 
 # Output should have a column named year, but year should not be treated as a variable
 def test_excludes_year_column_from_variables_in_output():
+    # Schema is not important here for checking data types, we are not comparing values
     df = pl.DataFrame(
         {"year": [2016, 2016, 2017, 2017], "x": [1, None, 3, 4], "y": [None, None, 5, 6]}
     )
