969b23ca5eff131d2d8998723b2f0ab97028215f
Sakina <slord.bhs@gmail.com>
2026-06-29 16:33:47 -0700

    check_na_rates: add mixed data test

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -32,6 +32,39 @@ def test_computes_correct_NA_rates_per_year():
     assert_frame_equal(result.sort(by=["variable", "year"]), expected.sort(by=["variable", "year"]))
 
 
+def test_computes_correct_NA_rates_per_year_on_mixed_data():
+    class LETTERS(pl.Enum):
+        a = "a"
+        b = "b"
+        c = "c"
+        d = "d"
+
+    df = pl.DataFrame(
+        {
+            "year": [2016, 2016, 2017, 2017],
+            "x": [LETTERS.a, None, LETTERS.c, LETTERS.d],
+            "y": [None, None, 5, 6],
+        }
+    )
+    result = check_na_rates(df)
+    expected = pl.DataFrame(
+        {
+            "variable": ["x", "y", "x", "y"],
+            "year": [2016, 2016, 2017, 2017],
+            "na_rate": [0.5, 1.0, 0.0, 0.0],
+            "n_total": [2, 2, 2, 2],
+        },
+        schema={
+            "variable": pl.Utf8,
+            "year": pl.Int64,
+            "na_rate": pl.Float64,
+            "n_total": pl.Int64,
+        },
+    )
+    # Ensure frames are comparing the correct rows to each other
+    assert_frame_equal(result.sort(by=["variable", "year"]), expected.sort(by=["variable", "year"]))
+
+
 # Output should have a column named year, but year should not be treated as a variable
 def test_excludes_year_column_from_variables_in_output():
     # Schema is not important here for checking data types, we are not comparing values
