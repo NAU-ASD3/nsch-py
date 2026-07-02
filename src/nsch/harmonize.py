2bd8fe7225f140b146b03f92d700b281ce20e7df
Sakina <slord.bhs@gmail.com>
2026-07-01 18:52:55 -0700

    transform_values: change loop parameters and type handling

--- src/nsch/harmonize.py
+++ src/nsch/harmonize.py
@@ -45,32 +45,34 @@ def transform_values(
 ) -> pl.LazyFrame:
     transformed_lf = lf  # Is this valid for the idea of not mutating in place?
     schema = transformed_lf.collect_schema()
-    variable_names = schema.names()
+    # Use a set for faster looping
+    variable_names = set(schema.names())
 
-    for transform_variable_name in transforms:
-        # if label in lf names and year in dict's year list
-        details = transforms[transform_variable_name]
-        transform_years = list(details["years"])
+    for transform_variable_name, details in transforms.items():
+        transform_years = details["years"]
         if (transform_variable_name in variable_names) & (str(year) in transform_years):
-            # access dictionary at that label
-            # create condition to map matching values
-            # true if the column value matches the old value in the transforms map
-            # and makes sure mapped values and column are of the same type
+            # Get colum datatype for casting back after transforming values
             column_dtype = schema[transform_variable_name]
-            condition = pl.col(transform_variable_name) == pl.lit(details["value"]).cast(
-                column_dtype
-            )
             label_col = str(transform_variable_name) + "_label"
+
+            # transform column to strings for comparison with string lists in transforms
+            column_as_string = pl.col(transform_variable_name).cast(pl.Utf8)
+            condition = column_as_string.is_in(details["value"])
+
             # Create new columns with the remapped values and labels, using when/then/otherwise
             value_transform = (
                 pl.when(condition)
-                .then(pl.lit(details["new_value"]).cast(column_dtype))
+                .then(
+                    column_as_string.replace(old=details["value"], new=details["new_value"]).cast(
+                        column_dtype
+                    )
+                )
                 .otherwise(pl.col(transform_variable_name))
                 .alias(transform_variable_name)
             )
             label_transform = (
                 pl.when(condition)
-                .then(pl.lit(details["new_label"]))
+                .then(column_as_string.replace(old=details["value"], new=details["new_label"]))
                 .otherwise(
                     pl.col(label_col)
                     if label_col in variable_names
@@ -78,6 +80,10 @@ def transform_values(
                 )
                 .alias(label_col)
             )
+            # if label column is not in variable_names, update list
+            if label_col not in variable_names:
+                # Variable_names is a set
+                variable_names.add(label_col)
             # modify whole column as a vector using the remap and add to return lf
             transformed_lf = transformed_lf.with_columns([value_transform, label_transform])
     return transformed_lf
